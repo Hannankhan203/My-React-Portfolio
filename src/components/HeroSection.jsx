6079732183be79615274b5d8c88ed77020e1ee53
Hannankhan203 <ak1818375@gmail.com>
2025-05-01 20:29:19 +0500

    Fixed Deployment issue

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,6 @@
-import React, { useState,  useEffect } from "react";
+import React from "react";
 
 function HeroSection({ darkMode }) {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 576);
 
   return (
     <section
