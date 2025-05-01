3a54e3ca407f24c5d38fe46590107f805542d877
Hannankhan203 <ak1818375@gmail.com>
2025-05-01 11:56:11 +0500

    Added links in Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,23 +6,34 @@ function Contact() {
     <footer id="Contact">
       <div className="contact-section">
         <div className="upper-section">
-        <h2 className="contact-heading">Contact Me</h2>
-        <p className="contact-p">
-          Got a project idea or just want to chat? Drop me a message!
-        </p>
-        <p className="mail">
-          <i className="fas fa-envelope"></i> Email: ak1818375@gmail.com
-        </p>
-        <p className="phone">
-          <i className="fas fa-phone"></i> Phone: +92 330 1375469
-        </p>
-        <div className="socials">
-          <i className="fab fa-github"></i>
-          <i className="fab fa-twitter"></i>
-          <i className="fab fa-instagram"></i>
+          <h2 className="contact-heading">Contact Me</h2>
+          <p className="contact-p">
+            Got a project idea or just want to chat? Drop me a message!
+          </p>
+          <p className="mail">
+            <i className="fas fa-envelope"></i> Email:{" "}
+            <a href="mailto:ak1818375@gmail.com">ak1818375@gmail.com</a>
+          </p>
+          <p className="phone">
+            <i className="fas fa-phone"></i> Phone:{" "}
+            <a href="tel:+923301375469">+92 330 1375469</a>
+          </p>
+          <div className="socials">
+            <a href="https://github.com/Hannankhan203">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://x.com/Hannankhan203">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://www.instagram.com/hannannnkhann/">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
         </div>
-        </div>
-        <p className="copyright">&copy; {new Date().getFullYear()} Abdul Hannan Khan. All rights reserved.</p>
+        <p className="copyright">
+          &copy; {new Date().getFullYear()} Abdul Hannan Khan. All rights
+          reserved.
+        </p>
       </div>
     </footer>
   );
