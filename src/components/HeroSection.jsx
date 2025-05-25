0b6b96eafbd2905bd0a8ac685f7bb8ddbc4279f9
Hannankhan203 <ak1818375@gmail.com>
2025-05-25 05:17:32 +0500

    Added GSAP Animation in Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from 'gsap';
 import { Link } from "react-router-dom";
 
 function HeroSection({ darkMode }) {
+  const headingRef = useRef(null);
+  const paraRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(headingRef.current, 
+      {
+        y: -400,
+        opacity: 0,
+      }, 
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+      }
+    )
+
+    gsap.fromTo(paraRef.current, 
+      {
+        x: -400,
+        opacity: 0,
+      }, 
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+      }
+    )
+
+    gsap.fromTo(buttonRef.current, 
+      {
+        y: 500,
+        opacity: 0,
+      }, 
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.8,
+        ease: "expo.out",
+      }
+    )
+  }, []);
   return (
     <section
       id="Hero-Section"
       className={darkMode ? "dark-mode" : "light-mode"}
     >
-      <h1 className={`hero-heading ${darkMode ? "dark-mode" : "light-mode"}`}>
+      <h1 ref={headingRef} className={`hero-heading ${darkMode ? "dark-mode" : "light-mode"}`}>
         Hi, I'm{" "}
         <span id="my-name" className={darkMode ? "dark-mode" : "light-mode"}>
           Abdul Hannan Khan
         </span>
       </h1>
-      <p className={`hero-p ${darkMode ? "dark-mode" : "light-mode"}`}>
+      <p ref={paraRef} className={`hero-p ${darkMode ? "dark-mode" : "light-mode"}`}>
         Front-End Developer | Creative Thinker | Responsive UI Builder | Growth
         Mindset
       </p>
-      <button
+      <button ref={buttonRef}
         className={`explore-btn ${darkMode ? "dark-mode" : "light-mode"}`}
         aria-label="Explore my projects"
       >
